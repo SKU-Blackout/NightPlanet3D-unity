57bb8d83d1f1a3db0a2272bc48fa8b4001659ffd
KIM JUNGHYUN <wjdgusrla125@gmail.com>
2022-12-28 21:56:40 +0900

    최종 빌드 파일 업로드

--- NightPlanet3D-unity/Assets/Scripts/Managers/Poolable.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Poolable : MonoBehaviour//오브젝트 풀링을 할 오브젝트인지 확인용도, 그 이외엔 기능없음
-{
-    public bool isUsing;
-}
--- NightPlanet3D-unity/Assets/Scripts/Potal/PotalBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PotalBase : MonoBehaviour
-{
-    public abstract void Next();
-}
